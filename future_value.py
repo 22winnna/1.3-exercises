913af758d20d55dd522addc39e7a5576be92b55e
22winnna <49483246+22winnna@users.noreply.github.com>
2019-11-21 09:42:02 -0500

    Update future_value.py

--- future_value.py
+++ future_value.py
@@ -20,10 +20,10 @@ def calculate_future_value(monthly_investment, yearly_interest, years):
     while choice.lower() == "y":
         # get input from the user
         monthly_investment = 0
-        while monthly_investment <= 0:
+        while monthly_investment <= 0 or monthly_investment >= 1000:
             monthly_investment = float(input("Enter monthly investment:\t"))
             if monthly_investment <= 0:
-                print("Entry must be greater than 0. Please try agian.")
+                print("Entry must be greater than 0 and less than or equal to 1000. Please try agian.")
 
         yearly_interest_rate = 0
         while yearly_interest_rate <= 0 or yearly_interest_rate > 15:
